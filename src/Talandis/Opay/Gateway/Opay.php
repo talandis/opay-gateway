d957aaa8defd199927e7889c07ef45139551200f
Tomas Talandis <tomas@talandis.lt>
2019-07-05 14:58:28 +0300

    add iso countries and update request parameter encoding

--- src/Talandis/Opay/Gateway/Opay.php
+++ src/Talandis/Opay/Gateway/Opay.php
@@ -132,18 +132,18 @@ class Opay implements CoreInterface, WebServiceInterface
         $str .= "
         <style type=\"text/css\">
 
-            html, body, form, p { margin: 0; padding: 0; }
+            html, body, form, p { margin: 0px; padding: 0px; }
             html, body { height: 100%; background: rgb(242, 242, 242); }
             h1 { font-size: 24px; font-weight: normal; color: rgb(68, 68, 68); margin:0; line-height: 100%; letter-spacing: normal; padding-bottom:24px;}
-            p { margin: 0 0 15px; }
+            p { margin: 0px 0px 15px; }
 
-            .clear { clear: both; display: block; height: 1px; overflow: hidden; margin: -1px 0 0; }
+            .clear { clear: both; display: block; height: 1px; overflow: hidden; margin: -1px 0px 0px; }
             body, div, p{ font-family: Arial, Helvetica, sans-serif; font-size: 14px; line-height: 150%; color: rgb(0, 0, 0); }
 
             #inprogress{padding: 50px 20px 0 20px;}
 
             .root { min-height: 100%; position: relative; text-align: center; background: rgb(242, 242, 242); }
-            #window { box-shadow: rgba(0, 0, 0, 0.2) 0 1px 5px; -webkit-box-shadow: rgba(0, 0, 0, 0.2) 0 1px 5px; width: 600px; margin: 70px auto 0; }
+            #window { box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 5px; -webkit-box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 5px; width: 600px; margin: 70px auto 0px; }
             .content { position: relative; background: rgb(255, 255, 255); }
             .posrel { position: relative; }
 
@@ -169,7 +169,7 @@ class Opay implements CoreInterface, WebServiceInterface
             #legal { position: relative; border-top-width: 1px; border-top-style: solid; border-top-color: rgb(234, 234, 234); padding: 30px 40px 15px; }
             #legal p, #legal strong { font-size: 12px; color: rgb(98, 98, 98); }
             #legal p a { font-size: 12px; color: rgb(98, 98, 98); text-decoration: none; border-bottom-width: 1px; border-bottom-style: solid; border-bottom-color: rgb(176, 176, 176); }
-            #legal p a:hover { border-bottom-width: 0; border-bottom-style: solid; border-bottom-color: rgb(176, 176, 176); }
+            #legal p a:hover { border-bottom-width: 0px; border-bottom-style: solid; border-bottom-color: rgb(176, 176, 176); }
 
             .cert {overflow:hidden; width:1px; height:1px;}
             
@@ -218,8 +218,8 @@ class Opay implements CoreInterface, WebServiceInterface
         }
         
         function getViewportWidth(){
-            var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0];
-            return w.innerWidth || e.clientWidth || g.clientWidth;    
+            var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0], x = w.innerWidth || e.clientWidth || g.clientWidth;
+            return x;    
         }
         
         function autoAlign(){    
@@ -311,7 +311,7 @@ class Opay implements CoreInterface, WebServiceInterface
 
     public function convertArrayOfParametersToEncodedString($parametersArray)
     {
-        return strtr(base64_encode(http_build_query($parametersArray)), array('+' => '-', '/' => '_', '=' => ','));
+        return strtr(base64_encode(http_build_query($parametersArray, '', '&', PHP_QUERY_RFC1738)), array('+' => '-', '/' => '_', '=' => ','));
     }
 
     public function convertEncodedStringToArrayOfParameters($encodedString)
@@ -410,7 +410,7 @@ class Opay implements CoreInterface, WebServiceInterface
                         throw new Exception('Could not decode JSON. json_decode() error code is ' . $jsonLastError, Exception::JSON_DECODING_ERROR);
                     }
                 } else {
-                    if ($data === NULL) {
+                    if ($data === null) {
                         throw new Exception('Wrong JSON format or the encoded data is deeper than the recursion limit.', Exception::JSON_DECODING_ERROR);
                     }
                 }
@@ -443,7 +443,7 @@ class Opay implements CoreInterface, WebServiceInterface
 
         $params = array(
             'http' => array(
-                'content' => http_build_query($parametersArray),
+                'content' => http_build_query($parametersArray, '', '&', PHP_QUERY_RFC1738),
                 'timeout' => $timeout
             )
         );
@@ -457,10 +457,10 @@ class Opay implements CoreInterface, WebServiceInterface
                     $parametersArray = array_merge($queryParamsArr, $parametersArray);
                 }
             }
-            $url .= '?' . http_build_query($parametersArray);
+            $url .= '?' . http_build_query($parametersArray, '', '&', PHP_QUERY_RFC1738);
         } else {
             $params['http']['method'] = 'POST';
-            $params['http']['content'] = http_build_query($parametersArray);
+            $params['http']['content'] = http_build_query($parametersArray, '', '&', PHP_QUERY_RFC1738);
         }
 
         $params['http']['timeout'] = $timeout;
@@ -498,33 +498,33 @@ class Opay implements CoreInterface, WebServiceInterface
         $arr = array(
             'aar' => 'aa', 'abk' => 'ab', 'ave' => 'ae', 'afr' => 'af', 'aka' => 'ak', 'amh' => 'am', 'arg' => 'an',
             'ara' => 'ar', 'asm' => 'as', 'ava' => 'av', 'aym' => 'ay', 'aze' => 'az', 'bak' => 'ba', 'bel' => 'be',
-            'bul' => 'bg', 'bis' => 'bi', 'bam' => 'bm', 'ben' => 'bn', 'bod' => 'bo',  'bre' => 'br',
+            'bul' => 'bg', 'bis' => 'bi', 'bam' => 'bm', 'ben' => 'bn', 'bod' => 'bo', 'bod' => 'bo', 'bre' => 'br',
             'bos' => 'bs', 'cat' => 'ca', 'che' => 'ce', 'cha' => 'ch', 'cos' => 'co', 'cre' => 'cr', 'ces' => 'cs',
-             'chu' => 'cu', 'chv' => 'cv', 'cym' => 'cy', 'dan' => 'da', 'deu' => 'de',
-             'div' => 'dv', 'dzo' => 'dz', 'ewe' => 'ee', 'ell' => 'el', 'eng' => 'en',
-            'epo' => 'eo', 'spa' => 'es', 'est' => 'et', 'eus' => 'eu', 'fas' => 'fa',
-            'ful' => 'ff', 'fin' => 'fi', 'fij' => 'fj', 'fao' => 'fo', 'fra' => 'fr', 'fry' => 'fy',
+            'ces' => 'cs', 'chu' => 'cu', 'chv' => 'cv', 'cym' => 'cy', 'cym' => 'cy', 'dan' => 'da', 'deu' => 'de',
+            'deu' => 'de', 'div' => 'dv', 'dzo' => 'dz', 'ewe' => 'ee', 'ell' => 'el', 'ell' => 'el', 'eng' => 'en',
+            'epo' => 'eo', 'spa' => 'es', 'est' => 'et', 'eus' => 'eu', 'eus' => 'eu', 'fas' => 'fa', 'fas' => 'fa',
+            'ful' => 'ff', 'fin' => 'fi', 'fij' => 'fj', 'fao' => 'fo', 'fra' => 'fr', 'fra' => 'fr', 'fry' => 'fy',
             'gle' => 'ga', 'gla' => 'gd', 'glg' => 'gl', 'grn' => 'gn', 'guj' => 'gu', 'glv' => 'gv', 'hau' => 'ha',
             'heb' => 'he', 'hin' => 'hi', 'hmo' => 'ho', 'hrv' => 'hr', 'hat' => 'ht', 'hun' => 'hu', 'hye' => 'hy',
-             'her' => 'hz', 'ina' => 'ia', 'ind' => 'id', 'ile' => 'ie', 'ibo' => 'ig', 'iii' => 'ii',
-            'ipk' => 'ik', 'ido' => 'io', 'isl' => 'is', 'ita' => 'it', 'iku' => 'iu', 'jpn' => 'ja',
-            'jav' => 'jv', 'kat' => 'ka',  'kon' => 'kg', 'kik' => 'ki', 'kua' => 'kj', 'kaz' => 'kk',
+            'hye' => 'hy', 'her' => 'hz', 'ina' => 'ia', 'ind' => 'id', 'ile' => 'ie', 'ibo' => 'ig', 'iii' => 'ii',
+            'ipk' => 'ik', 'ido' => 'io', 'isl' => 'is', 'isl' => 'is', 'ita' => 'it', 'iku' => 'iu', 'jpn' => 'ja',
+            'jav' => 'jv', 'kat' => 'ka', 'kat' => 'ka', 'kon' => 'kg', 'kik' => 'ki', 'kua' => 'kj', 'kaz' => 'kk',
             'kal' => 'kl', 'khm' => 'km', 'kan' => 'kn', 'kor' => 'ko', 'kau' => 'kr', 'kas' => 'ks', 'kur' => 'ku',
             'kom' => 'kv', 'cor' => 'kw', 'kir' => 'ky', 'lat' => 'la', 'ltz' => 'lb', 'lug' => 'lg', 'lim' => 'li',
             'lin' => 'ln', 'lao' => 'lo', 'lit' => 'lt', 'lub' => 'lu', 'lav' => 'lv', 'mlg' => 'mg', 'mah' => 'mh',
-            'mri' => 'mi',  'mkd' => 'mk', 'mal' => 'ml', 'mon' => 'mn', 'mar' => 'mr',
-            'msa' => 'ms', 'mlt' => 'mt', 'mya' => 'my', 'nau' => 'na', 'nob' => 'nb',
-            'nde' => 'nd', 'nep' => 'ne', 'ndo' => 'ng', 'nld' => 'nl', 'nno' => 'nn', 'nor' => 'no',
+            'mri' => 'mi', 'mri' => 'mi', 'mkd' => 'mk', 'mkd' => 'mk', 'mal' => 'ml', 'mon' => 'mn', 'mar' => 'mr',
+            'msa' => 'ms', 'msa' => 'ms', 'mlt' => 'mt', 'mya' => 'my', 'mya' => 'my', 'nau' => 'na', 'nob' => 'nb',
+            'nde' => 'nd', 'nep' => 'ne', 'ndo' => 'ng', 'nld' => 'nl', 'nld' => 'nl', 'nno' => 'nn', 'nor' => 'no',
             'nbl' => 'nr', 'nav' => 'nv', 'nya' => 'ny', 'oci' => 'oc', 'oji' => 'oj', 'orm' => 'om', 'ori' => 'or',
             'oss' => 'os', 'pan' => 'pa', 'pli' => 'pi', 'pol' => 'pl', 'pus' => 'ps', 'por' => 'pt', 'que' => 'qu',
-            'roh' => 'rm', 'run' => 'rn', 'ron' => 'ro', 'rus' => 'ru', 'kin' => 'rw', 'san' => 'sa',
-            'srd' => 'sc', 'snd' => 'sd', 'sme' => 'se', 'sag' => 'sg', 'sin' => 'si', 'slk' => 'sk',
-            'slv' => 'sl', 'smo' => 'sm', 'sna' => 'sn', 'som' => 'so', 'sqi' => 'sq', 'srp' => 'sr',
+            'roh' => 'rm', 'run' => 'rn', 'ron' => 'ro', 'ron' => 'ro', 'rus' => 'ru', 'kin' => 'rw', 'san' => 'sa',
+            'srd' => 'sc', 'snd' => 'sd', 'sme' => 'se', 'sag' => 'sg', 'sin' => 'si', 'slk' => 'sk', 'slk' => 'sk',
+            'slv' => 'sl', 'smo' => 'sm', 'sna' => 'sn', 'som' => 'so', 'sqi' => 'sq', 'sqi' => 'sq', 'srp' => 'sr',
             'ssw' => 'ss', 'sot' => 'st', 'sun' => 'su', 'swe' => 'sv', 'swa' => 'sw', 'tam' => 'ta', 'tel' => 'te',
             'tgk' => 'tg', 'tha' => 'th', 'tir' => 'ti', 'tuk' => 'tk', 'tgl' => 'tl', 'tsn' => 'tn', 'ton' => 'to',
             'tur' => 'tr', 'tso' => 'ts', 'tat' => 'tt', 'twi' => 'tw', 'tah' => 'ty', 'uig' => 'ug', 'ukr' => 'uk',
             'urd' => 'ur', 'uzb' => 'uz', 'ven' => 've', 'vie' => 'vi', 'vol' => 'vo', 'wln' => 'wa', 'wol' => 'wo',
-            'xho' => 'xh', 'yid' => 'yi', 'yor' => 'yo', 'zha' => 'za', 'zho' => 'zh', 'zul' => 'zu'
+            'xho' => 'xh', 'yid' => 'yi', 'yor' => 'yo', 'zha' => 'za', 'zho' => 'zh', 'zho' => 'zh', 'zul' => 'zu'
         );
         $languageCode = strtolower($languageCode);
         return (isset($arr[$languageCode])) ? $arr[$languageCode] : $languageCode;
